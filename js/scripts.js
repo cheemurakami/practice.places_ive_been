a52f60f283fea9c104c38ddab8feb242bc649c81
Chisato Murakami <kiwipom1031@gmail.com>
2020-04-09 16:38:29 -0700

    Add function to display landmark when add new places

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,27 @@
 //Album no logic
-function Album (){
+function Album (name){
   this.places = [];
   this.currentId = 0;
 };
+
 Album.prototype.addPlace = function(place){
+  place.id = this.addId();
   this.places.push(place);
 }
 Album.prototype.addId = function(){
-  this.currentId ++;
+  this.currentId += 1;
+  return this.currentId;
   //return this.currentId; nande?
 }
-
+Album.prototype.findPlace = function(id){
+  for(var i = 0; i<this.places.length; i++){
+    if(this.places[i].currentId == id){
+      return this.currentId;
+    } else {
+      return false;
+    };
+  }
+}
 //places no logic
 function Place (location, landmark, timeOfYear, notes){
   this.location = location;
@@ -19,7 +30,10 @@ function Place (location, landmark, timeOfYear, notes){
   this.notes = notes;
 };
 
-
+//after clicked add button, display the landmark only on the list
+function display(place){
+  $("#show").append("<li>" + place.landmark + "</li>");
+}
 
 
 
@@ -35,7 +49,10 @@ $(document).ready(function(){
     var notes = $("#notes").val();
     var place = new Place (location, landmark, timeOfYear, notes);
     album.addPlace(place);
-    album.addId(place);
-    console.log(album)
+    album.findPlace(place);
+    display(place);
+   
+    console.log(album);
+    console.log(place.id);
   });
 });
